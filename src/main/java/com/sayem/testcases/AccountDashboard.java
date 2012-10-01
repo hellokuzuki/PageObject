f3f664ea6fe84f6fe474bce49f027e5e57d974f4
sayems <syed@hotmail.com>
2012-10-01 16:31:40 -0400

    First Commit

---
+++ src/main/java/com/sayem/testcases/AccountDashboard.java
@@ -0,0 +1,35 @@
+package com.sayem.testcases;
+
+
+import com.sayem.pages.*;
+import com.sayem.util.TestUtil;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class AccountDashboard extends TestBase{
+    @Test
+    public void viewAllOrder(){
+        APPLICATION_LOGS.debug("Executing the upload pic test");
+
+        if(!TestUtil.isExecutable("viewAllOrder", xls))
+            throw new SkipException("Skipping the test");
+        LandingPage landingPage =null;
+        //go to landing page - if i am logged in else login
+        if(!isLoggedIn){
+            LoginPage lp = PageFactory.initElements(driver, LoginPage.class);
+            landingPage = lp.doLogin(CONFIG.getProperty("username"),CONFIG.getProperty("password"));
+        }else{
+            landingPage = getTopMenu().goToLandingPage();
+        }
+        AccountDashboardPage accountDashboardPage = landingPage.accountDashboard();
+        AddressBookPage addressBookPage = accountDashboardPage.manageAddresses();
+        AddNewAddressPage addNewAddressPage = addressBookPage.AddNewAddress();
+
+        // Keep this data in the Excel-sheet or something but do not hard-code it here!
+        addNewAddressPage.addNewAddressBook("Syed","Sayem", "3478789462", "32-48 30th St", "Astoria", "New York", "11106");
+
+        APPLICATION_LOGS.debug("Quitting driver");
+        quitDriver();
+    }
+}
--- src/main/java/com/sayem/testcases/AddressBook.java
+++ src/main/java/com/sayem/testcases/AddressBook.java
@@ -6,12 +6,38 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
-public class AddNewAddress extends TestBase{
+public class AddressBook extends TestBase{
+
     @Test
-    public void AddressBook(){
+    public void addNewAddress(){
         APPLICATION_LOGS.debug("Executing the upload pic test");
 
-        if(!TestUtil.isExecutable("addNewAddress", xls))
+        if(!TestUtil.isExecutable("AddNewAddress", xls))
+            throw new SkipException("Skipping the test");
+        LandingPage landingPage =null;
+        //go to landing page - if i am logged in else login
+        if(!isLoggedIn){
+            LoginPage lp = PageFactory.initElements(driver, LoginPage.class);
+            landingPage = lp.doLogin(CONFIG.getProperty("username"),CONFIG.getProperty("password"));
+        }else{
+            landingPage = getTopMenu().goToLandingPage();
+        }
+        AccountDashboardPage accountDashboardPage = landingPage.accountDashboard();
+        AddressBookPage addressBookPage = accountDashboardPage.manageAddresses();
+        AddNewAddressPage addNewAddressPage = addressBookPage.AddNewAddress();
+
+        // Keep this data in the Excel-sheet or something but do not hard-code it here!
+        addNewAddressPage.addNewAddressBook("Syed","Sayem", "3478789462", "32-48 30th St", "Astoria", "New York", "11106");
+
+        APPLICATION_LOGS.debug("Quitting driver");
+        quitDriver();
+    }
+
+    @Test
+    public void removeAddress(){
+        APPLICATION_LOGS.debug("Executing the upload pic test");
+
+        if(!TestUtil.isExecutable("RemoveAddress", xls))
             throw new SkipException("Skipping the test");
         LandingPage landingPage =null;
         //go to landing page - if i am logged in else login
